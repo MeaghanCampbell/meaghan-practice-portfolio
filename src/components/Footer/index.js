385064758233087013f61a5b5d45a35afdcb95e1
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-05-02 10:05:40 -0500

    footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Instagram from '../../assets/Instagram.png'
+import LinkedIn from '../../assets/Linkedin.png'
+import Github from '../../assets/Github.png'
 
 function Footer() {
     return (
         <footer>
-            <h2>WEB | DESIGN | BRANDING</h2>
+            <div className="icon-container">
+                <a href="https://github.com/MeaghanCampbell"><img className="icon" src={Github} alt="Github" /></a>
+                <a href="https://www.linkedin.com/in/meaghanmcnarycampbell/"><img className="icon" src={LinkedIn} alt="Linkedin" /></a>
+                <a href="https://instagram.com/meaghanmcbell"><img className="icon" src={Instagram} alt="Instagram" /></a>
+            </div>
             <h2>&copy; 2021 Meaghan Campbell. All rights reserved.</h2>
         </footer>
     )
