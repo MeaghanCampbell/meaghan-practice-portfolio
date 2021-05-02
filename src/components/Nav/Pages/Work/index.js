4b71b67fa49fe74de1a2f976aaacc8a09edb0a55
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-05-01 20:51:07 -0500

    work section

--- src/components/Nav/Pages/Work/index.js
+++ src/components/Nav/Pages/Work/index.js
@@ -1,25 +1,64 @@
 import React from 'react'
+import RockFitImage from '../../../../assets/rock-fit.jpg'
+import DiscLiveImage from '../../../../assets/disc-live.jpg'
+
 
 function Work() {
+
+    const workItems = [
+        {
+            id: 1,
+            name: "Disc Live",
+            image: DiscLiveImage,
+            description: "Front End application build with Server Side & Web API's",
+            repo: "https://github.com/MeaghanCampbell/disc-live",
+            app: "https://meaghancampbell.github.io/disc-live/"
+          },
+          {
+            id: 2,
+            name: "Rock Fit",
+            image: RockFitImage,
+            description: "Full Stack application built with Node, Express, Sequelize and MySQL",
+            repo: "https://github.com/MeaghanCampbell/rock-fit",
+            app: "https://rockfitapp.herokuapp.com/"
+          }
+    ]
+
     return (
         <section id="work" className="work">
             <h2>WORK</h2>
-                    <div className="work-items">
-                            <article className="disc-live">
-                                <a href="https://meaghancampbell.github.io/disc-live/" target="_blank" rel="noreferrer">
-                                    <h3>Disc Live</h3>
-                                    <p>Front-End Application using Server Side API's</p>
-                                    <a href="https://github.com/MeaghanCampbell/disc-live">View CODE</a>
-                                </a>
-                            </article>
-                            <article className="rock-fit">
-                                <a href="https://rockfitapp.herokuapp.com/" target="_blank" rel="noreferrer">
-                                    <h3>Rock Fit</h3>
-                                    <p>Full Stack Application using MySQL and Sequelize</p>
-                                    <a href="https://github.com/MeaghanCampbell/rock-fit">View CODE</a>
-                                </a>
-                            </article>
+            { workItems.map((projects) => (
+                <div 
+                    className="card"
+                    id={projects.id}
+                    key={projects.id}
+                    name={projects.name}
+                    image={projects.image}
+                    description={projects.description}
+                    repo={projects.repo}
+                    app={projects.app}
+                >
+                        <div className="img-container">
+                            <img className="workImage" alt={projects.name} src={projects.image} />
+                        </div>
+                        <div className="content">
+                            <ul>
+                            <li className="projectTitle">
+                                {projects.name}
+                            </li>
+                            <li>
+                                {projects.description}
+                            </li>
+                            <li>
+                                <a href={projects.app}>View The App</a>
+                            </li>
+                            <li>
+                                <a href={projects.repo}>View The Code</a>
+                            </li>
+                            </ul>
+                        </div>
                     </div>
+            ))}
         </section>
     )
 }
